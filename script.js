5732b98c1a91bb1af1b02add10da8e928be5ef72
Maaax02 <massiown@gmail.com>
2022-01-24 14:32:25 +0100

    added cycle to generate 10 emails

---
+++ script.js
@@ -0,0 +1,17 @@
+new Vue({
+    el: '#app',
+    data:{
+        email: []
+    },
+    methods:{
+
+    },
+
+    mounted(){
+        for (let i = 0; i <= 10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
+                this.email.push(result.data.response)
+            })
+        }
+    }
+})
