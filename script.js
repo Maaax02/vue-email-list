1625fe053c4659272f675f6e5d42b1f7d307ce0b
Maaax02 <massiown@gmail.com>
2022-01-24 18:08:37 +0100

    reduced cycle by one

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ new Vue({
     },
 
     mounted(){
-        for (let i = 0; i <= 10; i++){
+        for (let i = 0; i < 10; i++){
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
                 this.email.push(result.data.response)
             })
